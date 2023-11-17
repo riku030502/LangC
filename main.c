51387053698a6584d505ddc6b37d0ae10df0206d
riku030502 <147792717+riku030502@users.noreply.github.com>
2023-11-17 14:13:32 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void) 
+{
+    printf("Welcome to Programming!\n");
+    int x,y,z,price;
+    float m;
+    x=10;
+    y=2*x;
+    z=x+y;
+    printf("x=%d\ny=%d\nz=%d\n",x,y,z);
+    price=500;
+    m=price*1.1;
+    printf("%.1f\n",m);
+    return 0;
+
+}
